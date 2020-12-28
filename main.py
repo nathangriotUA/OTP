dd39f5b64c8fe82d80b25fabb61ea054b5281baa
Griot Nathan <nathan.griot@univ-avignon.fr>
2020-12-28 14:24:34 +0100

    base main file

---
+++ main.py
@@ -0,0 +1,13 @@
+import argparse
+import subprocess
+
+parser = argparse.ArgumentParser()
+
+
+def main():
+    ''' Main function '''
+    args = parser.parse_args()
+
+
+if __name__ == '__main__':
+    main()
