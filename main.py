a7e09c900456945265ed56a2970bb471386c9c91
Griot Nathan <nathan.griot@univ-avignon.fr>
2021-01-02 14:53:18 +0100

    deleteing files after they have been used

--- main.py
+++ main.py
@@ -30,6 +30,18 @@ def text_to_bin(text):
 
     return bins
 
+def readFromFile(filename):
+    ''' allows to read the content of a file:
+        input:
+            - filename
+        output:
+            - file content (str)
+    '''
+    file = open(filename, "r")
+    text = file.read()
+    file.close()
+    return text
+
 def generate_files(directory_name):
     ''' generate key files:
         input:
@@ -39,14 +51,22 @@ def generate_files(directory_name):
 
     i = 0
     dir_name = ""
+    try:
+        os.mkdir(directory_name)
+        os.mkdir(directory_name + "_receiver")
+    except OSError:
+            print ("unable to create directory")
 
+
+    created = False
     while i <= 9999:
         dir_name = str(i).zfill(4)
         try:
-            os.mkdir(directory_name+"/"+dir_name)
+            os.mkdir(directory_name + "/" + dir_name)
+            created = True
         except OSError:
-            print (dir_name+" not available")
-        else:
+            print (dir_name + " not available")
+        if created:
             break
         i += 1
 
@@ -56,19 +76,21 @@ def generate_files(directory_name):
         data_c = [bin(int.from_bytes(random_dev.read(1), 'big'))[2:].zfill(8) for i in range(2000)]
 
         cur_file = str(i).zfill(2)
-        with open(directory_name+'/'+dir_name+"/"+cur_file+"p", 'w') as f:
+        with open(directory_name + '/' + dir_name + "/" + cur_file + "p", 'w') as f:
             for random_bits in data_p:
                 f.write(str(random_bits))
-        with open(directory_name+'/'+dir_name+"/"+cur_file+"s", 'w') as f:
+        with open(directory_name + '/' + dir_name + "/" + cur_file + "s", 'w') as f:
             for random_bits in data_s:
                 f.write(str(random_bits))
-        with open(directory_name+'/'+dir_name+"/"+cur_file+"c", 'w') as f:
+        with open(directory_name + '/' + dir_name + "/" + cur_file + "c", 'w') as f:
             for random_bits in data_c:
                 f.write(str(random_bits))
+
+    subprocess.call('cp -r ./' + directory_name + '/* ./' + directory_name + '_receiver/', shell=True)
     random_dev.close()
         
 
-def transmission(message,directory_name):
+def transmission(message, directory_name):
     ''' transmission it will take a message and encrypt it
     input:
         - message (str)
@@ -85,25 +107,30 @@ def transmission(message,directory_name):
         while j <= 99 and found == False : 
             dir = str(i).zfill(4)
             filename = str(j).zfill(2)
-            if os.path.isfile(directory_name+'/'+dir+'/'+filename+"c"):
+            if os.path.isfile(directory_name + '/' + dir + '/' + filename + "c"):
                 dir_used = dir
                 file_used = filename
                 found = True
             j += 1
         i += 1
     
+    # If not found then we return and print an error message
+    if found == False:
+        print("Sorry please generate files in order to do this task.")
+        return 0
+
     if len(message) > 2000:
         SystemError("Message is too long")
 
-    f = open(directory_name+'/'+dir_used+"/"+file_used+"p", "r")
+    f = open(directory_name + '/' + dir_used + "/" + file_used + "p", "r")
     datap = f.read()
     datap = [datap[i : i + 8] for i in range(0, len(datap), 8)]
     f.close()
-    f = open(directory_name+'/'+dir_used+"/"+file_used+"s", "r")
+    f = open(directory_name + '/' + dir_used + "/" + file_used + "s", "r")
     datas = f.read()
     datas = [datas[i : i + 8] for i in range(0, len(datas), 8)]
     f.close()
-    f = open(directory_name+'/'+dir_used+"/"+file_used+"c", "r")
+    f = open(directory_name + '/' + dir_used + "/" + file_used + "c", "r")
     datac = f.read()
     datac = [datac[i : i + 8] for i in range(0, len(datac), 8)]
     f.close()
@@ -115,7 +142,7 @@ def transmission(message,directory_name):
         for i, data in enumerate(message):
             print(data)
             a = int(data, 2)
-            b = int(datac[i] , 2)
+            b = int(datac[i], 2)
             c = a + b
             data = bin(c)[2:].zfill(9)
             f.write(data)
@@ -123,6 +150,7 @@ def transmission(message,directory_name):
         for data in datas:
             f.write(str(data))
 
+    os.remove(directory_name+'/'+dir_used+"/"+file_used+"c") # remove file after writing the message
     return 0
 
 
@@ -137,7 +165,7 @@ def receive(directory_name,file):
         output:
             - message 
      '''
-    i= 0
+    i = 0
     dir_used = ""
     file_used = ""
     found = False
@@ -150,31 +178,36 @@ def receive(directory_name,file):
         while j <= 99 and found == False : 
             dir = str(i).zfill(4)
             filename = str(j).zfill(2)
-            f = open(directory_name+'/'+dir+"/"+filename+"p", "r")
+            f = open(directory_name + '/' + dir + "/" + filename + "p", "r")
             datap = f.read()
             f.close()
             if datat == datap:
-                f = open(directory_name+'/'+dir+"/"+filename+"c", "r")
+                # pad found so we delete it
+                os.remove(directory_name + '/' + dir + "/" + filename + "p")
+                f = open(directory_name + '/' + dir + "/"+ filename + "c", "r")
                 datac = f.read()
                 datac = [datac[i : i + 8] for i in range(0, len(datac), 8)]
                 dir_used = dir
                 file_used = filename
                 f.close()
+                # transimission recovered so we delete it
+                os.remove(directory_name + '/' + dir + "/" + filename + "c")
                 found = True
             j += 1
         i += 1
+
 
     f = open(file, "r")
     datam = f.read()
     datam = datam[384:-384]
     datam = [datam[i : i + 9] for i in range(0, len(datam), 9)]
 
-    with open(directory_name+'-'+dir_used+"-"+file_used+"m", 'w') as f:
+    with open(directory_name + '-' + dir_used + "-" + file_used + "m", 'w') as f:
         for i, data in enumerate(datam):
             a = int(data, 2)
             b = int(datac[i], 2)
             c = a - b
-            print(a-b)
+            print(a - b)
             data = chr(c)
             f.write(data)
     return 0
